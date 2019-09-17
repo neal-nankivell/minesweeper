fc6c87a61898559638c10831aa5c5e3eab890c93
neal.nankivell <neal.nankivell@gmail.com>
2019-09-17 13:12:18 +0200

    Added stories for the main App entry point Added fieldset and legend for the config editor

--- src/components/ConfigurationEditor/ConfigurationEditor.tsx
+++ src/components/ConfigurationEditor/ConfigurationEditor.tsx
@@ -5,49 +5,52 @@ export const ConfigurationEditor: React.FC<
   ConfigurationEditorProps
 > = props => (
   <form key="config-form">
-    <label htmlFor="height">Height</label>
-    <input
-      disabled={props.disabled}
-      name="height"
-      onChange={e =>
-        props.updateConfiguration(
-          e.target.valueAsNumber,
-          props.width,
-          props.mineCount
-        )
-      }
-      value={props.height}
-      type="number"
-    />
+    <fieldset>
+      <legend>Configurtation:</legend>
+      <label htmlFor="height">Height</label>
+      <input
+        disabled={props.disabled}
+        name="height"
+        onChange={e =>
+          props.updateConfiguration(
+            e.target.valueAsNumber,
+            props.width,
+            props.mineCount
+          )
+        }
+        value={props.height}
+        type="number"
+      />
 
-    <label htmlFor="width">Width</label>
-    <input
-      name="width"
-      disabled={props.disabled}
-      onChange={e =>
-        props.updateConfiguration(
-          props.height,
-          e.target.valueAsNumber,
-          props.mineCount
-        )
-      }
-      value={props.width}
-      type="number"
-    />
+      <label htmlFor="width">Width</label>
+      <input
+        name="width"
+        disabled={props.disabled}
+        onChange={e =>
+          props.updateConfiguration(
+            props.height,
+            e.target.valueAsNumber,
+            props.mineCount
+          )
+        }
+        value={props.width}
+        type="number"
+      />
 
-    <label htmlFor="mines">Mines</label>
-    <input
-      name="mines"
-      disabled={props.disabled}
-      onChange={e =>
-        props.updateConfiguration(
-          props.height,
-          props.width,
-          e.target.valueAsNumber
-        )
-      }
-      value={props.mineCount}
-      type="number"
-    />
+      <label htmlFor="mines">Mines</label>
+      <input
+        name="mines"
+        disabled={props.disabled}
+        onChange={e =>
+          props.updateConfiguration(
+            props.height,
+            props.width,
+            e.target.valueAsNumber
+          )
+        }
+        value={props.mineCount}
+        type="number"
+      />
+    </fieldset>
   </form>
 );
