2ba60823331ab821170c282e08a21f610973b880
neal.nankivell <neal.nankivell@gmail.com>
2019-09-18 10:56:58 +0200

    Cleaned up the Typography in the Configuration editor stories.

--- src/components/ConfigurationEditor/ConfigurationEditor.stories.tsx
+++ src/components/ConfigurationEditor/ConfigurationEditor.stories.tsx
@@ -4,6 +4,7 @@ import { ConfigurationEditor } from "./ConfigurationEditor";
 import { number, boolean } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 import ConfigurationEditorContainer from "./ConfigurationEditorContainer";
+import { Typography, Container, Paper } from "@material-ui/core";
 
 storiesOf("Components/Configuration Editor", module)
   .add("Basic States", () => {
@@ -20,15 +21,23 @@ storiesOf("Components/Configuration Editor", module)
   .add("Redux Container", () => (
     <>
       <ConfigurationEditorContainer />
-      <hr />
-      <h4>Notes:</h4>
-      <p>
-        The above component will reflect updates to the configuration from the
-        knobs below.
-      </p>
-      <p>
-        But the knobs configuration does not render updates to state changes in
-        the store caused by this component.
-      </p>
+      <Container maxWidth="sm">
+        <Paper style={{ padding: "2em", margin: "2em" }}>
+          <Typography variant="subtitle1" gutterBottom>
+            Notes:
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            The above component will reflect updates to the configuration from
+            the knobs below.
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            But the knobs configuration does not render updates to state changes
+            in the store caused by this component.
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            To enable editing the Game Phase will need to be set to Setup.
+          </Typography>
+        </Paper>
+      </Container>
     </>
   ));
